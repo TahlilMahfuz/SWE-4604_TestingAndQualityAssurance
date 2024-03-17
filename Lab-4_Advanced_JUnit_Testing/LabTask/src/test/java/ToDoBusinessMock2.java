eee2fdcfcf3ca6aa10ae1b81e08e79a500639a22
Tahlil_Mahfuz <tahlilkfaiyaz@gmail.com>
2024-03-17 20:39:38 +0600

    Uploaded some files

---
+++ Lab-4_Advanced_JUnit_Testing/LabTask/src/test/java/ToDoBusinessMock2.java
@@ -0,0 +1,28 @@
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import labMocking.ToDoBusiness;
+import labMocking.ToDoService;
+import org.mockito.Mock;
+
+public class ToDoBusinessMock2 {
+@Test
+public void deleteTodosusing() {
+	ToDoService todoService = mock(ToDoService.class); 
+	 List<String> combinedlist = Arrays.asList("Use Hibernate Java", "Use Hibernate Core", "Use Hibernate", "Use Spring MVC");  
+     when(todoService.getTodos("dummy")).thenReturn(combinedlist);
+     ToDoBusiness business = new ToDoBusiness(todoService);  
+     
+     
+     business.deleteTodosNotRelatedToHibernate("dummy");
+     verify(todoService).deleteTodos("Use Spring MVC");
+     
+}
+
+}
